f49a87349fc1ffdc890e80f8d0dd82d97558ba7a
tanverified <62833134+tanverified@users.noreply.github.com>
2024-01-14 21:52:03 +0530

    add project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css'
+
+function App() {
+  return (
+    <div className='app'>
+    <h1>App</h1>
+    <img src="./nature.jpg" alt="nature"/>
+    <button>Submit</button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const appContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(appContainer);
+root.render(<App />);
